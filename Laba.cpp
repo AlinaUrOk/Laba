ba5c119fa1fc4a341475daa39b7e5d47e8c8ff02
AlinaUrOk <71850840+AlinaUrOk@users.noreply.github.com>
2020-10-20 10:27:03 +0300

    Add files via upload

---
+++ Laba.cpp
@@ -0,0 +1,32 @@
+﻿// Laba.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    /*4. Реалізувати переведення числа з системи числення з основою m<10 у десяткову.*/
+    setlocale(LC_ALL, "Russian");
+    int m, x, y = 0, res = 0;
+    cout << "Введите целое число: " << endl;
+    cin >> x;
+    cout << "Введите основание системы счисления m<10:" << endl;
+    cin >> m;
+    if (m < 10)
+    {
+        while (x > 0)
+        {
+            res += (x % 10) * pow(m, y);
+            x = x / 10;
+            y++;
+        }
+        cout << res;
+    }
+    else
+    {
+        cout << "Число введено неверно! " << endl;
+    }
+    return 0;
+}
+
---
+++ Laba2.cpp
@@ -0,0 +1,43 @@
+﻿// Laba2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+/*20. Число є паліндромом, якщо воно читається однаково і зліва направо і справа наліво. Знайти
+всі числа – паліндроми, менші за N.*/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std; 
+bool isPalindrom ( int n )
+{
+    bool f = true;
+    vector <int>a(0);
+    while (n != 0)
+    {
+        a.push_back(n % 10);
+        n /= 10;
+        for (int i = 0; i < a.size(); i++) 
+        {
+            if (a[i] != a[a.size() - 1 - i])
+            {
+                f = false;
+                break;
+            }
+        }
+    }
+        return f;
+    }
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int N, Palindrom;
+    cout << "Введите предел для палиндрома:";
+    cin >> N;
+    for (int n = 1; n <= N; n++)
+    {
+        Palindrom = isPalindrom(N);
+        if (Palindrom == isPalindrom(n));
+        {
+        cout << "isPalindrom-" << n << endl;
+        }
+    }
+    return 0;
+}
